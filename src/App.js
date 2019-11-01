757eeb6a8d414a7539bf3207d0fa57b03856eb8d
Ioan Zicu <ioan.zicu94@gmail.com>
2019-11-01 19:11:56 +0100

    Animate onMouseEnter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { useState } from 'react'
 import logo from './logo.png'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [toggleLogo, setToggleLogo] = useState(true)
+  const [cards, setCards] = useState([
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7 },
+    { id: 8 },
+    { id: 9 }
+  ])
 
   function toggleLogoEvent(event) {
     setToggleLogo(!toggleLogo)
@@ -14,11 +26,17 @@ function App() {
       <header className='App-header'>
         <img
           src={logo}
-          className={toggleLogo ? 'static-logo' : 'animated-logo'}
-          onClick={toggleLogoEvent}
+          className={toggleLogo ? 'static-logo' : 'static-logo animated jello'}
+          onMouseEnter={toggleLogoEvent}
+          onMouseLeave={toggleLogoEvent}
           alt='logo'
         />
       </header>
+      <div className='Grid'>
+        {cards.map(card => (
+          <Card duration={150} key={card.id} />
+        ))}
+      </div>
     </div>
   )
 }
