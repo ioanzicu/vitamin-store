4def213f3a6189b8ca4ae2ad4583b92764c0d6db
Ioan Zicu <ioan.zicu94@gmail.com>
2019-11-01 18:39:56 +0100

    Spin logo on click

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import logo from './logo.png'
+import './App.css'
 
 function App() {
+  const [toggleLogo, setToggleLogo] = useState(true)
+
+  function toggleLogoEvent(event) {
+    setToggleLogo(!toggleLogo)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img
+          src={logo}
+          className={toggleLogo ? 'static-logo' : 'animated-logo'}
+          onClick={toggleLogoEvent}
+          alt='logo'
+        />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
