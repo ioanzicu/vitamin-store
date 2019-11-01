e46d29f3c2bed50ff5e62cb1180bb709211e0281
Ioan Zicu <ioan.zicu94@gmail.com>
2019-11-02 00:48:07 +0100

    improve ui

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,14 +2,17 @@ import React from 'react'
 import './Loading.css'
 
 const Loading = () => (
-  <div className='sk-chase'>
-    <div className='sk-chase-dot'></div>
-    <div className='sk-chase-dot'></div>
-    <div className='sk-chase-dot'></div>
-    <div className='sk-chase-dot'></div>
-    <div className='sk-chase-dot'></div>
-    <div className='sk-chase-dot'></div>
-  </div>
+  <>
+    <div className='sk-chase'>
+      <div className='sk-chase-dot'></div>
+      <div className='sk-chase-dot'></div>
+      <div className='sk-chase-dot'></div>
+      <div className='sk-chase-dot'></div>
+      <div className='sk-chase-dot'></div>
+      <div className='sk-chase-dot'></div>
+    </div>
+    <h1>Once these circles disappear, begin scrolling!</h1>
+  </>
 )
 
 export default Loading
